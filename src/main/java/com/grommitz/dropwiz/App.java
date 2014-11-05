5ac24ea46d06506c551c8f091f0ab12e8d10e3a2
grommitz <martincharlesworth@gmail.com>
2014-11-05 22:39:41 +0000

    weld looks like a lost cause

--- src/main/java/com/grommitz/dropwiz/App.java
+++ src/main/java/com/grommitz/dropwiz/App.java
@@ -2,8 +2,8 @@ package com.grommitz.dropwiz;
 
 import javax.inject.Inject;
 
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
+//import org.jboss.weld.environment.se.Weld;
+//import org.jboss.weld.environment.se.WeldContainer;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
@@ -15,14 +15,15 @@ import com.grommitz.dropwiz.health.TemplateHealthCheck;
 public class App extends Application<AppConfig> {
 
 	@Inject NameService service;
-	@Inject HelloWorldResource resource;
+//	@Inject HelloWorldResource resource;
 
 	public static void main(String[] args) throws Exception {
-		Weld weld = new Weld();
-		WeldContainer container = weld.initialize();
-		App app = container.instance().select(App.class).get();	    
+		//Weld weld = new Weld();
+		//WeldContainer container = weld.initialize();
+		//App app = container.instance().select(App.class).get();	    
+		App app = new App();
 		app.run(args);
-		weld.shutdown();
+		//weld.shutdown();
 	}
 
 	@Override
@@ -32,14 +33,14 @@ public class App extends Application<AppConfig> {
 
 	@Override
 	public void initialize(Bootstrap<AppConfig> bootstrap) {
-		System.out.println("Initialised app for " + service.getName());
+		//System.out.println("Initialised app for " + service.getName());
 	}
 	
 	@Override
 	public void run(AppConfig configuration, Environment environment) {
-//		final HelloWorldResource resource = new HelloWorldResource(
-//				configuration.getTemplate(),
-//				configuration.getDefaultName());
+		final HelloWorldResource resource = new HelloWorldResource(
+				configuration.getTemplate(),
+				configuration.getDefaultName());
 		environment.jersey().register(resource);
 
 		final TemplateHealthCheck healthCheck =
--- src/main/java/com/grommitz/dropwiz/WeldApp.java
+++ src/main/java/com/grommitz/dropwiz/WeldApp.java
@@ -2,23 +2,23 @@ package com.grommitz.dropwiz;
 
 import javax.inject.Inject;
 
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
+//import org.jboss.weld.environment.se.Weld;
+//import org.jboss.weld.environment.se.WeldContainer;
 
 public class WeldApp {
 	
-	@Inject NameService service;
+//	@Inject NameService service;
+//
+//	public static void main(String[] args) {
+//		Weld weld = new Weld();
+//		WeldContainer container = weld.initialize();
+//		WeldApp app = container.instance().select(WeldApp.class).get();
+//		app.run();
+//		weld.shutdown();
+//	}
 
-	public static void main(String[] args) {
-		Weld weld = new Weld();
-		WeldContainer container = weld.initialize();
-		WeldApp app = container.instance().select(WeldApp.class).get();
-		app.run();
-		weld.shutdown();
-	}
-
-	public void run() {
-		System.out.println("HELLO " + service.getName());
-	}
+//	public void run() {
+//		System.out.println("HELLO " + service.getName());
+//	}
 	
 }
